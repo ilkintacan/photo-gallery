ae1241077f5a137232896aa603a5b1411765cac8
ILKIN TACAN <ilkin.tacan@gmail.com>
2018-11-02 14:14:10 +0300

    adding ionic pro

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ 
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Pro } from '@ionic/pro';
+
+Pro.init('ff1de2dd', {
+  appVersion: '0.0.1'
+ });
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
